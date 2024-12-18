361f98258c93b7ebd9beae6df523c06c05a6ef99
Thatsvie <viepaulalmt@gmail.com>
2024-12-18 11:24:53 -0600

    updated csv, improved contrast for no articles found, updated filterbar for uvc articles

--- frontend/src/components/FilterBar.js
+++ frontend/src/components/FilterBar.js
@@ -31,14 +31,11 @@ const FilterBar = ({ filters, setFilters, clearFilters, onSearch }) => (
         <option value="">Category</option>
         <option value="Advocacy and Resources">Advocacy and Resources</option>
         <option value="Children and Long COVID">Children and Long COVID</option>
-        <option value="Indoor Air Quality and Filtration">
-          Indoor Air Quality and Filtration
-        </option>
+        <option value="Far-UVC">Far-UVC</option>
+        <option value="Indoor Air Quality and Filtration">Indoor Air Quality and Filtration</option>
         <option value="Long COVID">Long COVID</option>
         <option value="Masking">Masking</option>
-        <option value="Prevention and Treatment">
-          Prevention and Treatment
-        </option>
+        <option value="Prevention and Treatment">Prevention and Treatment</option>
         <option value="Public Health">Public Health</option>
         <option value="Pending Categorization">Pending Categorization</option>
       </select>
